f7ccdbe3bda4a16cf8e9dabe69e1ab72aef5d16f
Zunon <zunon@zunon.io>
2018-12-02 07:36:45 +0400

    Get location via GPS

--- app/src/main/java/ae/ac/ku/pizza/SignUpActivity.java
+++ app/src/main/java/ae/ac/ku/pizza/SignUpActivity.java
@@ -1,13 +1,24 @@
 package ae.ac.ku.pizza;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class SignUpActivity extends Activity {
+
+  String location = "Unknown";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +46,8 @@ public class SignUpActivity extends Activity {
       floor = floorE.getText().toString(),
       apartment = apartmentE.getText().toString();
 
+    getLocation();
+
     SharedPreferences localPrefs = getSharedPreferences(getString(R.string.shared_preferences_filename), MODE_PRIVATE);
     SharedPreferences.Editor editor = localPrefs.edit();
 
@@ -45,13 +58,48 @@ public class SignUpActivity extends Activity {
     editor.putString(getString(R.string.building_key), building);
     editor.putString(getString(R.string.floor_key), floor);
     editor.putString(getString(R.string.apartment_key), apartment);
+    editor.putString(getString(R.string.location_key), location);
 
     editor.apply();
-
     Intent done = new Intent(this, MainActivity.class);
     done.putExtra(MainActivity.SIGNUP, true);
     startActivity(done);
   }
 
   // TODO: Add Google Maps API support.
+
+  private void getLocation() {
+    if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 69);
+    }
+
+    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    LocationListener locationListener = new LocationListener() {
+      @Override
+      public void onLocationChanged(Location location) {
+        setLocation(location.getLatitude() + ", " + location.getLongitude());
+      }
+
+      @Override
+      public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Zunon", "onStatusChanged");
+      }
+
+      @Override
+      public void onProviderEnabled(String provider) {
+        Log.d("Zunon", "onProviderEnabled");
+      }
+
+      @Override
+      public void onProviderDisabled(String provider) {
+        Log.d("Zunon", "onProviderDisabled");
+      }
+    };
+
+    locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener, null);
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
 }
